0859cb456b6160dd3411c35fc3d7829faf5f2ad4
mudshoes <kg082108@wgcloud.org>
2023-10-26 09:42:54 -0500

    Added more explanatory comments

--- code.py
+++ code.py
@@ -15,7 +15,12 @@ while True:
 
     ## Prints the amount of euros
     print("Euros: " + str(euro))
-
+    
+    '''
+    Asks if the user would like to continue.
+    If the answer is "no", "0", or "n" it breaks the loop
+    Otherwise the loop continues.
+    '''
     answer = input("Would you like to convert dollars to euros? ")
     if answer == 'no' or '0' or 'n':
         break
