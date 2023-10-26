e8de6488f8852a683dc14ea613576be2f86fac1a
mudshoes <kg082108@wgcloud.org>
2023-10-26 09:22:27 -0500

    I just made all the code

---
+++ code.py
@@ -0,0 +1,12 @@
+## No takey my code. is mine
+print("US dollar to Euro converter")
+print("Made by Mudshoes. All rights reserved. 2023")
+
+## Gets the dollar amount from the user
+dollar = float(input("How many dollars are there?"))
+
+##converts dollars to euros
+euro = dollar * 0.94540
+
+## Prints the amount of euros
+print("Euros: " + str(euro))
