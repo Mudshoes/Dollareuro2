aa45f69a522959eeac65664a369c7e4f49201c4f
mudshoes <kg082108@wgcloud.org>
2023-10-26 09:31:54 -0500

    Put everything in a while loop

--- code.py
+++ code.py
@@ -2,11 +2,18 @@
 print("US dollar to Euro converter")
 print("Made by Mudshoes. All rights reserved. 2023")
 
-## Gets the dollar amount from the user
-dollar = float(input("How many dollars are there?"))
+## When answer = no or 0, the while loop stops
+answer = 'yes'
+while True:
+    ## Gets the dollar amount from the user
+    dollar = float(input("How many dollars are there?"))
 
-##converts dollars to euros
-euro = dollar * 0.94540
+    ##converts dollars to euros
+    euro = dollar * 0.94540
 
-## Prints the amount of euros
-print("Euros: " + str(euro))
+    ## Prints the amount of euros
+    print("Euros: " + str(euro))
+
+    answer = input("Continue conversions?")
+    if answer == 'no' or '0' or 'n':
+        break
